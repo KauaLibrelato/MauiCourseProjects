49b0daf091bf61c11704ba0d6276f2d62b2a578c
KauaLibrelato <kaualibrelatodacosta@gmail.com>
2024-05-16 11:07:34 -0300

    finish appNumeroDaSorte

---
+++ AppNumeroDaSorte/AppNumeroDaSorte/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppNumeroDaSorte {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            //Current page = PaginaAtual
+            MainPage = new MainPage();
+        }
+    }
+}
